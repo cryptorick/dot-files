194e472dedd1fb6a51bdb74cf00bb9c5f04fdbd7
cryptorick <cryptorick@users.noreply.github.com>
2019-01-26 11:59:32 -0500

    Separate straight.el bits from package.el bits

---
+++ .emacs.d-straight/straight/build-cache.el
@@ -0,0 +1,16 @@
+
+:betab
+
+gnu/bsd
+
+"26.1"
+
+#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ("straight" ("2019-01-26 01:18:53-0500" nil (:type git :host github :repo "raxod502/straight.el" :branch "master" :package "straight" :local-repo "straight.el"))))
+
+#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ())
+
+#s(hash-table size 65 test eq rehash-size 1.5 rehash-threshold 0.8125 data ())
+
+("straight")
+
+t
