ef5eb9c10dd7f471290256b66ee9cd944d598678
cryptorick <cryptorick@users.noreply.github.com>
2019-03-19 00:38:55 -0400

    Add emms config (works on fbsd at least)

--- .emacs.d-straight/init.el
+++ .emacs.d-straight/init.el
@@ -432,6 +432,30 @@ Source: https://www.emacswiki.org/emacs/UnfillRegion"
    ;; on how to store that password in an authinfo file.)
 )
 
+(use-package emms
+  :straight t
+  :config
+  (require 'emms-setup)
+  (require 'emms-player-mpd)
+  (emms-all) ; don't change this to values you see on stackoverflow questions if you expect emms to work
+  (setq emms-seek-seconds 5)
+  (setq emms-player-list '(emms-player-mpd))
+  (setq emms-info-functions '(emms-info-mpd))
+  (setq emms-volume-change-function #'emms-volume-mpd-change)
+  (require 'mpc) ; already included in Emacs; require probably unnecessary.
+  (setq mpc-host (expand-file-name "~/.mpd/socket"))
+  (setq emms-player-mpd-server-nameq (expand-file-name "~/.mpd/socket")) ; default: "localhost"
+  (setq emms-player-mpd-server-port nil)                                 ; default: "6600"
+  ;;(setq emms-source-file-default-directory (expand-file-name "~/Music/"))
+  :bind
+  ("s-m p" . emms)
+  ("s-m b" . emms-smart-browse)
+  ("s-m r" . emms-player-mpd-update-all-reset-cache)
+  ("<XF86AudioPrev>" . emms-previous)
+  ("<XF86AudioNext>" . emms-next)
+  ("<XF86AudioPlay>" . emms-pause)
+  ("<XF86AudioStop>" . emms-stop))
+
 ;;----------------------------------------------------------------------
 ;; exwm
 
